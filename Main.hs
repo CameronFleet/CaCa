306521d6894d2472221712eb2762d4ecb5b2ebb3
CameronFleet1998 <Cameron-Fleet@hotmail.com>
2018-03-09 21:24:33 +0000

    Fully Commented

--- Main.hs
+++ Main.hs
@@ -10,46 +10,13 @@ type Tables = [(Relation, Table)]
 -- Representation of all the information for a single Relational Symbol
 data Table = Column String [String] | Columns String [String] Table deriving Show
 
--- EVAL, The Whole evaluation of the AST
-
--- -- Evals a Program
--- eval :: Program -> Table -> String
--- eval (Program (FromGetExpr fromGet vars)) table = evalFromGetExpr fromGet vars table
--- eval (Program (FromGetWhere fromGet equals vars)) _ = ""
-
--- -- Evals a From _ Get; Expression 
--- evalFromGetExpr :: FromGet -> Vars -> Table -> String
--- evalFromGetExpr _ asVars table = evalAsVars asVars table
-
--- -- Evals a As _; Expression the 'as' vars, so eg. as name1, name 2 would return the string of : show(name1) ++ "," ++ show(name2)
--- evalAsVars :: Vars -> Table -> String 
--- evalAsVars vars table = evalAsVars' (varsToString vars) table
+-- ================================================================  EVAL  ===========================================================================================
+eval :: Program -> Table -> String
+eval (Program (FromGetExpr fromGet vars)) table = evalFromGetExpr fromGet vars table
+eval (Program (FromGetWhere fromGet equals vars)) _ = ""
 
 
--- evalAsVars' :: [String] -> Table -> String
--- evalAsVars' (v:[]) (Columns name (content:contents) table)   | v==name = content
---                                                              | otherwise = evalAsVars' [v] table
-
--- evalAsVars' (v:[]) (Column name (content:contents))          | v==name = content
---                                                              | otherwise = ""
-
--- evalAsVars' (v:vars) (Columns name (content:contents) table) | v==name = content ++ "," ++ evalAsVars' vars table
---                                                              | otherwise = (evalAsVars' [v] table) ++ "," ++ (evalAsVars' vars (Columns name (content:contents) table))
-
--- evalAsVars' (v:vars) (Column name (content:contents))        | v==name = content
---                                                              | otherwise = ""
-
--- INFORMATION ABOUT THE AST, e.g the relational symbols, the amount of vars.. 
-
--- Gets A SINGLE Relational symbol
--- getRelation :: Program -> String
--- getRelation (Program (FromGetExpr fromGet _)) = getRelation' fromGet
--- getRelation (Program (FromGetWhere fromGet _ _)) = getRelation' fromGet
-
--- getRelation' :: FromGet -> String 
--- getRelation' (FromGet (Relation symbol) _) = symbol
--- getRelation' (FromGetAnd (Relation symbol) _ _) = symbol
-
+-- =============================================================  TABLE MAKERS  =======================================================================================
 -- Get FILEPATHS , returns FILEPATHS 
 getFilePaths :: Program -> [FilePath]
 
@@ -63,28 +30,6 @@ getVars (Program _ ) = []
 -- Parameter $2: Output of GetVars, the Relation, in order, with the String assignments
 makeTables :: [String] -> [(Relation,[String])] -> Tables 
 makeTables _ _ = []
-
--- Returns Designated Variables
--- getVars :: Program -> [String] 
--- getVars (Program (FromGetExpr fromGet _)) = getVars' fromGet
-
--- getVars' :: FromGet -> [String]
--- getVars' (FromGet _ vars) = getVars'' vars
--- getVars' (FromGetAnd _ vars _ ) = getVars'' vars
-
--- getVars'' :: ToGet -> [String]
--- getVars'' (Params (Some s)) = [s]
--- getVars'' (Params1 (Var s)) = [s]
--- getVars'' (Params2 toget1 toget2) = getVars'' toget1 ++ getVars'' toget2
-
--- -- Generation of the Table Structure
--- makeTable :: String -> [String]-> Int -> Table
--- makeTable string vars number = makeTable' (clean (wordsWhen (==',') string)) vars number
-
--- makeTable' :: [String]-> [String] -> Int -> Table
--- makeTable' (x:xs) (y:ys) number | number > 1  = Columns y [x] (makeTable' xs ys (number-1))
--- makeTable' (x:xs) (y:ys) number | number == 1 = Column y [x]
-
 
 
 -- ================================================================  AUX  ===========================================================================================
@@ -108,7 +53,7 @@ wordsWhen p s =  case dropWhile p s of
                             where (w, s'') = break p s'
 
 
--- IO Functions 
+-- ================================================================  IO  ===========================================================================================
 
 readFiles :: [FilePath] -> IO [String]
 readFiles ss = mapM readFile ss 
@@ -116,8 +61,7 @@ readFiles ss = mapM readFile ss
 filepath :: String -> FilePath
 filepath s = (s ++ ".csv")
 
--- MAIN 
-
+-- ================================================================  MAIN  =========================================================================================== 
 main :: IO ()
 main = do
     s <- getContents
