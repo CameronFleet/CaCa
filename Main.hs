ecb3b1acefe87e063dde1289a9fbc3934a6fa94f
CameronFleet1998 <Cameron-Fleet@hotmail.com>
2018-03-09 18:21:47 +0000

    Added MAIN

--- Main.hs
+++ Main.hs
@@ -15,42 +15,44 @@ import Data.Char
 -- C.csv 1,2,3 \n 4,5,6
 -- Columns (MoreInfo ("1", (Info "4")), Columns ( MoreInfo ("2", (Info "5")), (Column (MoreInfo ("3", (Info "6"))))))
 
--- Columns ()
 
+-- Type of all of the Relations and their corrosponding Tables 
+type Tables = [(Relation, Table)]
 
--- 
+-- Representation of all the information for a single Relational Symbol
 data Table = Column String [String] | Columns String [String] Table deriving Show
 
--- EVAL
+-- EVAL, The Whole evaluation of the AST
 
+-- Evals a Program
 eval :: Program -> Table -> String
 eval (Program (FromGetExpr fromGet vars)) table = evalFromGetExpr fromGet vars table
 eval (Program (FromGetWhere fromGet equals vars)) _ = ""
 
+-- Evals a From _ Get; Expression 
 evalFromGetExpr :: FromGet -> Vars -> Table -> String
 evalFromGetExpr _ asVars table = evalAsVars asVars table
 
+-- Evals a As _; Expression the 'as' vars, so eg. as name1, name 2 would return the string of : show(name1) ++ "," ++ show(name2)
 evalAsVars :: Vars -> Table -> String 
-evalAsVars vars table = smth (evalAsVars' vars) table
-
-evalAsVars' :: Vars -> [String]
-evalAsVars' (Vars1 (Var s)) = [s]
-evalAsVars' (Vars2 (Var s) vars) = [s] ++ (evalAsVars' vars)
-
-smth :: [String] -> Table -> String
-smth (v:[]) (Columns name (content:contents) table)   | v==name = content
-                                                      | otherwise = smth [v] table
-smth (v:[]) (Column name (content:contents))          | v==name = content
-                                                      | otherwise = ""
-smth (v:vars) (Columns name (content:contents) table) | v==name = content ++ "," ++ smth vars table
-                                                      | otherwise = (smth [v] table) ++ "," ++ (smth vars (Columns name (content:contents) table))
-smth (v:vars) (Column name (content:contents))        | v==name = content
-                                                      | otherwise = ""
+evalAsVars vars table = evalAsVars' (varsToString vars) table
 
 
+evalAsVars' :: [String] -> Table -> String
+evalAsVars' (v:[]) (Columns name (content:contents) table)   | v==name = content
+                                                             | otherwise = evalAsVars' [v] table
+
+evalAsVars' (v:[]) (Column name (content:contents))          | v==name = content
+                                                             | otherwise = ""
+
+evalAsVars' (v:vars) (Columns name (content:contents) table) | v==name = content ++ "," ++ evalAsVars' vars table
+                                                             | otherwise = (evalAsVars' [v] table) ++ "," ++ (evalAsVars' vars (Columns name (content:contents) table))
+
+evalAsVars' (v:vars) (Column name (content:contents))        | v==name = content
+                                                             | otherwise = ""
 
 
--- SMTH
+-- INFORMATION ABOUT THE AST, e.g the relational symbols, the amount of vars.. 
 
 -- Gets A SINGLE Relational symbol
 getRelation :: Program -> String
@@ -61,7 +63,7 @@ getRelation' :: FromGet -> String
 getRelation' (FromGet (Relation symbol) _) = symbol
 getRelation' (FromGetAnd (Relation symbol) _ _) = symbol
 
--- Gets the Variables designated
+-- Returns Designated Variables
 getVars :: Program -> [String]
 getVars (Program (FromGetExpr fromGet _)) = getVars' fromGet
 
@@ -74,7 +76,7 @@ getVars'' (Params (Some s)) = [s]
 getVars'' (Params1 (Var s)) = [s]
 getVars'' (Params2 toget1 toget2) = getVars'' toget1 ++ getVars'' toget2
 
-
+-- Generation of the Table Structure
 makeTable :: String -> [String]-> Int -> Table
 makeTable string vars number = makeTable' (clean (wordsWhen (==',') string)) vars number
 
@@ -85,10 +87,19 @@ makeTable' (x:xs) (y:ys) number | number == 1 = Column y [x]
 
 
 -- AUX 
+
+-- Turns the Data Type:  Vars ====> [String] ; Retains order
+varsToString :: Vars -> [String]
+varsToString (Vars1 (Var s)) = [s]
+varsToString (Vars2 (Var s) vars) = [s] ++ (varsToString vars)
+
+
+-- Takes a list of CSV e.g : ["Bob", "Alice", "John\n"]  ====> ["Bob", "Alice", "John"]
 clean :: [String] -> [String]
 clean (x:[]) = wordsWhen (=='\n') x
 clean (x:xs) = wordsWhen (=='\n') x ++ clean xs
 
+-- Acts like a SplitOn Function  , wordsWhen (==',') "This,Is,A,CSV" =====> ["This", "Is", "A" , "CSV"]
 wordsWhen     :: (Char -> Bool) -> String -> [String]
 wordsWhen p s =  case dropWhile p s of
                       "" -> []
